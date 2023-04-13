c40a3f487d405149d44f1149da2191667d8eccb4
lucas sales <lucas2013sales@hotmail.com>
2023-04-13 04:08:48 -0300

    mais exercicios

---
+++ exercicio.-js/fundamentos/objeto.js
@@ -0,0 +1,19 @@
+const prod1 = {}
+prod1.nome = 'Celular Ultra Mega'
+prod1.preco = 4998.90
+prod1['Desconto Legal'] = 0.40
+
+console.log(prod1)
+
+const prod2 ={
+    nome: 'Camisa polo',
+    preco : 79.90,
+    obj:{
+        jeans:'apertada',
+        obj:{
+            saia:'rosa'
+        }
+    }
+
+}
+console.log(prod2)
