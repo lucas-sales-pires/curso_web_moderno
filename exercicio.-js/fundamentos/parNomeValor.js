03a9ac396478c4fc498145af00a3f20e7809198e
lucas sales <lucas2013sales@hotmail.com>
2023-04-18 09:08:51 -0300

    Update parNomeValor.js

--- exercicio.-js/fundamentos/parNomeValor.js
+++ exercicio.-js/fundamentos/parNomeValor.js
@@ -3,5 +3,19 @@ const saudacao = 'Opa' //contexto léxico 1
 
 function exec(){
     const saudacao = 'Falaaa' //contexto léxico2
+    return saudacao
+}
+//Objetos são grupos aninhados de pares nome/valor
+const cliente ={
+    nome:'Pedro',
+    idade:32,
+    peso:90,
+    endereco:{
+        logradouro:'Rua Muito Legal',
+        numero:123
+    }
 }
 
+console.log(exec())
+console.log(saudacao)
+console.log(cliente.endereco.logradouro)
