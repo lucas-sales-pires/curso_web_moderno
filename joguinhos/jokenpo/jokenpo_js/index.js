d85d6c63da4169a7b0bbc84bb0173ae410737918
lucas sales <lucas2013sales@hotmail.com>
2023-05-01 12:56:00 -0300

    jokenpo 1.0

--- joguinhos/jokenpo/jokenpo_js/index.js
+++ joguinhos/jokenpo/jokenpo_js/index.js
@@ -6,6 +6,12 @@ let tesoura = document.getElementById('tesoura')
 let opcoes = document.getElementsByClassName('opcao')
 let lista =['pedra','papel','tesoura']
 let jogadaM = document.getElementById('jogadaM')
+let pontuacaovc = document.getElementById('pontuacaovc')
+let pontuacaomaq = document.getElementById('pontuacaomaq')
+let contavc = 0
+let contamaq = 0
+
+
 
 
 function gerarNumeroAleatorio(minimo, maximo) {
@@ -21,14 +27,20 @@ function selecionado() {
     if(pedra.checked){
         resposta.innerHTML = ''
         resposta.innerHTML +='Você escolheu <strong>PEDRA</strong>'
+        
         if(JogadaM() == 'tesoura'){
+            contavc+=1
             resposta.innerHTML +=' E a Maquina escolheu <strong>TESOURA</strong>,<br> Você ganhou Parabéns !'
             jogadaMaq.src ='./imagens/tesoura.png' 
+            pontuacaovc.innerHTML =contavc
+            
 
 
         }else if(JogadaM() == 'papel'){
+            contamaq+=1
             resposta.innerHTML +=' E a Maquina escolheu <strong>PAPEL</strong>,<br> Infelizmente você perdeu !'
             jogadaMaq.src ='./imagens/papel.png' 
+            pontuacaomaq.innerHTML = contamaq
   
         }else{
             resposta.innerHTML += ' <strong>EMPATE !</strong>'
@@ -38,14 +50,19 @@ function selecionado() {
         resposta.innerHTML = ''
         resposta.innerHTML += 'Você escolheu <strong>PAPEL</strong>'
         if(JogadaM() == 'tesoura'){
-            resposta.innerHTML +=' E a maquina escolheu  <strong>TESOURA</strong> ,<br> Você infelizmente perdeu !'
+            contamaq+=1
+            resposta.innerHTML +=' E a maquina escolheu <strong>TESOURA</strong> ,<br> Você infelizmente perdeu !'
             jogadaMaq.src ='./imagens/tesoura.png'
+            pontuacaomaq.innerHTML =contamaq
+
         }else if(JogadaM() == 'papel'){
             resposta.innerHTML +=' E a Maquina escolheu <strong>PAPEL</strong>,<br><strong>EMPATE</strong>'
             jogadaMaq.src ='./imagens/papel.png'
         }else{
+            contavc+=1
             resposta.innerHTML +=' E a Maquina escolheu <strong>PEDRA</strong>,<br>Parabéns você ganhou !'
             jogadaMaq.src = './imagens/pedra.png'
+            pontuacaovc.innerHTML =contavc
         }
     }else if(tesoura.checked){
         resposta.innerHTML = ''
@@ -56,9 +73,12 @@ function selecionado() {
         }else if(JogadaM() == 'papel'){
             resposta.innerHTML +=' E a Maquina escolheu <strong>PAPEL</strong>,<br>Parabéns ! Você ganhou !'
             jogadaMaq.src ='./imagens/papel.png'
+            pontuacaovc.innerHTML =contavc
         }else{
-            resposta.innerHTML +=' E a Maquina escolheu <strong>PEDRA</strong>,<br>Infelimente você perdeu !'
+            contamaq+=1
+            resposta.innerHTML +=' E a Maquina escolheu <strong>PEDRA</strong>,<br>Infelizmente você perdeu !'
             jogadaMaq.src = './imagens/pedra.png'
+            pontuacaomaq.innerHTML = contamaq
         }
         
     }
