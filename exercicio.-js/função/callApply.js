2383de26f53270c3eb6d9e26d61354a69005f557
lucas sales <lucas2013sales@hotmail.com>
2023-05-12 04:20:55 -0300

    call e apply

--- exercicio.-js/função/callApply.js
+++ exercicio.-js/função/callApply.js
@@ -17,5 +17,5 @@ const carro = {preco:49990,desc: 0.20}
 console.log(getPreco.call(carro))
 console.log(getPreco.apply(carro))
 
-console.log(getPreco.call(carro,0.17,'$'))
-console.log(getPreco.apply(carro,[0.17,'$']))
+console.log(getPreco.call(carro,0.17,'R$'))
+console.log(getPreco.apply(carro,[0.17,'R$']))
--- exercicio.-js/função/teste.js
+++ exercicio.-js/função/teste.js
@@ -1,5 +1,4 @@
-lista = [1,2,3,4,5,6]
-for(let x of lista){
-    lista+=x
-    console.log(lista)
+function transforma(valor){
+    console.log('R$ '+ valor.toFixed(2))
 }
+console.log(transforma(5253.8555))
