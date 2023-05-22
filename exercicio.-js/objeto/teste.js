ca23721e7567ab64fac224cb93ec1e9fd9347f23
lucas sales <lucas2013sales@hotmail.com>
2023-05-22 04:46:21 -0300

    heranças

--- exercicio.-js/objeto/teste.js
+++ exercicio.-js/objeto/teste.js
@@ -1,7 +1,20 @@
-function horarioAtual() {
-    let dataAtual = new Date(); //variável recebe a data atual
-    let hora = dataAtual.toLocaleTimeString(); // variável recebe a data atual convertida para hora atual
-    console.log(hora); //hora será mostrada no console
+class Pessoa {
+  constructor(nome, idade, sexo) {
+     this.nome = nome;
+     this.idade = idade;
+     this.sexo = sexo;
   }
-  setTimeout(horarioAtual, 3000);
-  horarioAtual()
+}
+
+class Matheus extends Pessoa {
+  constructor(nomeDiferente, algumaIdade, meuSobrenome) {
+     super(nomeDiferente, algumaIdade); 
+     this.sobrenome = meuSobrenome;
+  }
+}
+pessoa1 = new Pessoa('lucas',25,'M')
+pessoa2 = new Matheus('Julio',30,'Sales')
+pessoa2.sexo='M'
+console.log(pessoa1)
+
+console.log(pessoa2)
