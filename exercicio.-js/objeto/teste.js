815b28a5f90123c794fd11e165d986d74a952513
lucas sales <lucas2013sales@hotmail.com>
2023-05-26 19:53:23 -0300

    mais estudos

--- exercicio.-js/objeto/teste.js
+++ exercicio.-js/objeto/teste.js
@@ -1,20 +1,3 @@
-class Pessoa {
-  constructor(nome, idade, sexo) {
-     this.nome = nome;
-     this.idade = idade;
-     this.sexo = sexo;
-  }
-}
-
-class Matheus extends Pessoa {
-  constructor(nomeDiferente, algumaIdade, meuSobrenome) {
-     super(nomeDiferente, algumaIdade); 
-     this.sobrenome = meuSobrenome;
-  }
-}
-pessoa1 = new Pessoa('lucas',25,'M')
-pessoa2 = new Matheus('Julio',30,'Sales')
-pessoa2.sexo='M'
-console.log(pessoa1)
-
-console.log(pessoa2)
+const lista = [1,2,3]
+const lista2 = [...lista]
+console.log(lista2)
