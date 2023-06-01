9b32b1052ed977b8ec54d5a13810df8cbf63af94
lucas sales <lucas2013sales@hotmail.com>
2023-06-01 19:24:52 -0300

    beleza
    beleza cara

--- exercicio.-js/função/teste.js
+++
@@ -1,4 +0,0 @@
-let data1 = new Date().getDay()
-let data2 = new Date().getDay()
-let resultado = (data1.getTime() - data2.getTime())/(24*3600)
-console.log(resultado)
---
+++ exercicio.-js/objeto/teste.js
@@ -0,0 +1,4 @@
+let lista = ['lucas','fatima','diego']
+let lista2 = []
+
+console.log(lista.map(n => n.toUpperCase()))
