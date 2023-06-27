80f53017f7ad25b7b740bc7553fb000596b0c123
lucas sales <lucas2013sales@hotmail.com>
2023-06-27 04:48:33 -0300

    exercicios

--- exercicio.-js/node/funcionarios/funcionarios.js
+++ exercicio.-js/node/funcionarios/funcionarios.js
@@ -1,21 +1,22 @@
+// url
 const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json';
+//biblioteca axis
+const axios = require('axios')
+//filtro por chineses
+const chineses = f => f.pais === 'China'
+//filtro por genêro feminino
+const mulheres = f => f.genero === 'F'
+//
+const menorSalario = (func,funcAtual) =>{
+  return func.salario < funcAtual.salario ? func : funcAtual
+}
 
-fetch(url)
-  .then(response => response.json())
-  .then(data => {
-    const funcionarios = data;
-    let menor = Number.MAX_VALUE;
-    let funcionarioMenorSalario;
+axios.get(url).then(response =>{
+  const funcionarios = response.data
+  const func = funcionarios
+  .filter(chineses)
+  .filter(mulheres)
+  .reduce(menorSalario)
+  console.log(func)
+})
 
-    funcionarios.forEach(f => {
-      if (f.salario < menor) {
-        menor = f.salario;
-        funcionarioMenorSalario = f;
-      }
-    });
-
-    console.log('Employee with the lowest salary:', funcionarioMenorSalario);
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
---
+++ exercicio.-js/node/instanciaNova.js
@@ -0,0 +1,10 @@
+// Uma factory retorna uma nova instância
+
+module.exports = () => {
+    return {
+        valor: 1,
+        inc(){
+            this.valor++
+        }
+    }
+}
